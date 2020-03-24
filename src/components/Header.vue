82cb4988cbdb328d86a885df5c6090cc06f10853
lucianotamanaha <lucianotamanaha@gmail.com>
2020-03-23 23:28:53 -0300

    Router + stocks

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-app-bar dark>
+      <v-toolbar-title>
+        <v-btn text to="/">APP Stock Trade</v-btn>
+      </v-toolbar-title>
+      <v-btn text small to="/portfolio">Portfolio</v-btn>
+      <v-btn text small to="/stocks">Stocks</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn text small>End Day</v-btn>
+      <div>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn color="primary" small dark v-on="on">Save & Load</v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, index) in sublinks" :key="index">
+              <v-list-item-content>
+                <v-list-item-title v-text="item.name"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    sublinks: [
+      { name: "teste", link: "/teste" },
+      { name: "teste", link: "/teste" }
+    ]
+  })
+};
+</script>
---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-col cols="4">
+    <v-card class="mx-auto" max-width="344" outlined>
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="overline mb-4">{{stock.name}}</div>
+          <v-list-item-title class="headline mb-1">Price: {{stock.price}}</v-list-item-title>
+          <v-list-item-subtitle>
+            <v-text-field type="number" label="Quantity" filled v-model="quantity"></v-text-field>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-list-item-avatar tile size="80" color="grey">
+          <v-img :src="stock.image"></v-img>
+        </v-list-item-avatar>
+      </v-list-item>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="success" @click="buyStock" :disabled="quantity <= 0">Buy</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: ['stock'],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+      buyStock() {
+          const order = {
+              stockId: this.stock.id,
+              stockPrice: this.stock.price,
+              quantity: this.quantity
+          }
+          console.log(order);
+          this.quantity = 0;
+      }
+  }
+};
+</script>
