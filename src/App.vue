3e8f068d589517c9842989d04a87b976507caaaa
lucianotamanaha <lucianotamanaha@gmail.com>
2020-03-25 12:28:21 -0300

    Animation transition rotes

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <app-header></app-header>
     <v-content>
       <v-container fluid>
-        <router-view></router-view>
+        <transition name="slide">
+          <router-view></router-view>
+        </transition>
       </v-container>
     </v-content>
 
@@ -14,14 +16,43 @@
 </template>
 
 <script>
-  import AppHeader from './components/Header'
+import AppHeader from "./components/Header";
 
-  export default {
-    components: {
-      AppHeader
-    },
-    created() {
-      this.$store.dispatch('initStocks');
-    }
+export default {
+  components: {
+    AppHeader
+  },
+  created() {
+    this.$store.dispatch("initStocks");
   }
+};
 </script>
+
+<style scoped>
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+}
+</style>
